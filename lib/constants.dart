5acb56cc6a9c653742c7e63590b593b48c34b0ce
Tharindu <tharinduonline1080@gmail.com>
2025-04-13 23:10:26 +0530

    Removed male female, Added all basic features ✅

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kNumberColor1 = Colors.green;
+
+//BMI Comment colors
+
+//underweight
+const kUnderWeight = Colors.yellow;
+
+//Normal
+const kNormalWeight = Colors.green;
+
+//Overweight
+const kOverWeight = Colors.orange;
+
+//Obese
+const kObeseWeight = Colors.red;
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -29,91 +31,151 @@ class _MainPageState extends State<MainPage> {
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 40.0),
-                  child: Row(
-                    //male female row
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.only(
-                          left: 35.0,
-                        ),
-                        child: Column(
-                          //male
-                          children: [
-                            Icon(
-                              Icons.male,
-                              size: 100,
-                            ),
-                            Text(
-                              "Male",
-                              style: TextStyle(fontSize: 20),
-                            )
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Padding(
-                        padding: EdgeInsets.only(right: 35.0),
-                        child: Column(
-                          //female
-                          children: [
-                            Icon(
-                              Icons.female,
-                              size: 100,
-                            ),
-                            Text(
-                              "Female",
-                              style: TextStyle(fontSize: 20),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
                 Row(
                   //height weight row
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(left: 40.0),
+                      padding: const EdgeInsets.only(left: 25.0),
                       child: Column(
                         //Height
                         children: [
-                          Text("Height",style: TextStyle(fontSize: 20, ),),
-                          
                           Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("176", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Text(
+                              "Height(cm)",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, bottom: 10, right: 10),
+                            child: Text(height.toString(),
+                                style: TextStyle(
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.bold,
+                                    color: kNumberColor1)),
                           ),
                           Row(
                             children: [
-                              Icon(
-                                Icons.remove,
-                                size: 40,
+                              Padding(
+                                padding: const EdgeInsets.only(right: 15.0),
+                                child: FloatingActionButton(
+                                  onPressed: onHeightMinus,
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                  ),
+                                  backgroundColor: Colors.red,
+                                ),
                               ),
-                              Icon(
-                                Icons.add,
-                                size: 40,
+                              Padding(
+                                padding: const EdgeInsets.only(left: 5.0),
+                                child: FloatingActionButton(
+                                  onPressed: onHeightPlus,
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),
+                                  backgroundColor: Colors.blue,
+                                ),
                               )
                             ],
                           )
                         ],
                       ),
                     ),
-                    Column(
-                        //weight
-                        )
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50.0),
+                      child: Column(
+                        //Height
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Text(
+                              "Mass(Kg)",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, bottom: 10, right: 10),
+                            child: Text(
+                              mass.toString(),
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                  color: kNumberColor1),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 15.0),
+                                child: FloatingActionButton(
+                                  onPressed: onMassMinus,
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                  ),
+                                  backgroundColor: Colors.red,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 5.0),
+                                child: FloatingActionButton(
+                                  onPressed: onMassPlus,
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                  ),
+                                  backgroundColor: Colors.blue,
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
                 Row(
                   //BMI value row
                   children: [
-                    Column(
-                        //BMI TEXT
-
-                        //Result value
-                        )
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 135.0, top: 50),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 5.0),
+                              child: Text(
+                                "BMI Value",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Text(
+                              bmi.toString(),
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.bold,
+                                  color: kNumberColor1),
+                            ),
+                            Text(
+                              comment,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: kNumberColor1),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 )
               ],
@@ -121,4 +183,47 @@ class _MainPageState extends State<MainPage> {
           )),
     );
   }
+
+  double height = 176;
+  double mass = 65;
+  double bmi = 0;
+  String comment = "";
+
+  void onHeightMinus() {
+    print("H minus");
+    setState(() {
+      height--;
+    });
+  }
+
+  void onHeightPlus() {
+    print("H plus");
+    setState(() {
+      height--;
+    });
+  }
+
+  void onMassMinus() {
+    print("M minus");
+    setState(() {
+      mass--;
+    });
+  }
+
+  void onMassPlus() {
+    print("M plus");
+    setState(() {
+      mass++;
+    });
+  }
+
+  void reset() {
+    print("reset");
+    setState(() {
+      height = 176;
+      mass = 65;
+    });
+  }
+
+  void cal() {}
 }
