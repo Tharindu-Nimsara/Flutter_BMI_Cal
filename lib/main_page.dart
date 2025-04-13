fca7a19f61cc58005dd58eb33358aee88057d4f6
Tharindu <tharinduonline1080@gmail.com>
2025-04-13 13:49:54 +0530

    made some components 👽

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -12,46 +12,113 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Row(
-              //male female row
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Center(
+          child: Text(
+            "BMI Calculator",
+            style: TextStyle(
+                fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+      body: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
               children: [
-                Column(
-                  //male
-                  children: [Text(" Micon"), Text("male")],
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40.0),
+                  child: Row(
+                    //male female row
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: 35.0,
+                        ),
+                        child: Column(
+                          //male
+                          children: [
+                            Icon(
+                              Icons.male,
+                              size: 100,
+                            ),
+                            Text(
+                              "Male",
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: EdgeInsets.only(right: 35.0),
+                        child: Column(
+                          //female
+                          children: [
+                            Icon(
+                              Icons.female,
+                              size: 100,
+                            ),
+                            Text(
+                              "Female",
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                Column(
-                  //female
-                  children: [Text("ficon"), Text("female")],
+                Row(
+                  //height weight row
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40.0),
+                      child: Column(
+                        //Height
+                        children: [
+                          Text("Height",style: TextStyle(fontSize: 20, ),),
+                          
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("176", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.remove,
+                                size: 40,
+                              ),
+                              Icon(
+                                Icons.add,
+                                size: 40,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Column(
+                        //weight
+                        )
+                  ],
+                ),
+                Row(
+                  //BMI value row
+                  children: [
+                    Column(
+                        //BMI TEXT
+
+                        //Result value
+                        )
+                  ],
                 )
               ],
             ),
-            Row(
-              //height weight row
-              children: [
-                Column(
-                    //Height
-
-                    ),
-                Column(
-                    //weight
-                    )
-              ],
-            ),
-            Row(
-              //BMI value row
-              children: [
-                Column(
-                    //BMI TEXT
-
-                    //Result value
-                    )
-              ],
-            )
-          ],
-        ));
+          )),
+    );
   }
 }
